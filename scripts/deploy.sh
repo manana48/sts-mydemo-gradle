11025e362d8b1d8114aa844f7db2002375a98487
manana48 <manana48@naver.com>
2024-11-05 16:44:27 +0900

    new deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 ## Initialize log file
 cat /dev/null > /home/ec2-user/deploy.log
+cat /dev/null > /home/ec2-user/deploy_err.log
 
 ## Deploy Java Service
 BUILD_JAR=$(ls /home/ec2-user/mydemo-deploy/build/libs/sbb-0.0.1-SNAPSHOT.jar)
@@ -20,10 +21,10 @@ then
   echo "> 실행중인 애플리케이션이 없으므로 종료하지 않음" >> /home/ec2-user/deploy.log
 else
   echo "> kill -9 $CURRENT_PID" >> /home/ec2-user/deploy.log
-  kill -9 $CURRENT_PID
+  kill -9 $CURRENT_PID 2>>/home/ec2-user/deploy_err.log
   sleep 10;
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>>/home/ec2-user/deploy_err.log &
