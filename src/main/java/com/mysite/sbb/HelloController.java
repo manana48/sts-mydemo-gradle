91505321a32ea9a7153a57ad49d4be3b1cd7dcb4
manana48 <manana48@naver.com>
2024-11-26 14:36:33 +0900

    new deploy v1

--- src/main/java/com/mysite/sbb/HelloController.java
+++ src/main/java/com/mysite/sbb/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
-        return "Hello Spring Boot Board v433";
+        return "Hello Spring Boot Board v1";
     }
 }
